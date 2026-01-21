fa2ef041c9f50c99ca21d682eecac8bb73eccd39
Vegetable-SYC <109536807+Vegetable-SYC@users.noreply.github.com>
2026-01-21 11:39:22 +0800

    update Miner

--- NerdMiner-FNK0103/NerdMiner-FNK0103F(2.8'' ILI9341 TN).bat
+++ NerdMiner-FNK0103/NerdMiner-FNK0103F(2.8'' ILI9341 TN).bat
@@ -1,4 +1,14 @@
 @echo off
-cd /d "%~dp0Code"
-python FNK0103F.py
+
+:start
+
+echo.
+
+esptool.exe --chip esp32 --baud 921600 --before default-reset --after hard-reset --no-stub write-flash -z --flash-mode dio --flash-freq 40m --flash-size detect 0x0000 ./Bin/FNK0103F/merged_output.bin
+
+echo.
+echo press enter to continue burned!
+echo.
 pause
+
+goto start
